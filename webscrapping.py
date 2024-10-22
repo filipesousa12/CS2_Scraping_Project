fb6f724e70ddc1fe6f630af8cab864024a6357c9
filipesousa12 <filipe.sousa12@hotmail.com>
2024-10-22 18:44:42 +0100

    adding dockerfile
    Signed-off-by: filipesousa12 <filipe.sousa12@hotmail.com>

--- webscrapping.py
+++ webscrapping.py
@@ -17,6 +17,9 @@ def connection():
     # Set up firefox options to run in headless mode
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--disable-gpu")   
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-extensions")
     driver = webdriver.Firefox(options=options)
 
     return driver
